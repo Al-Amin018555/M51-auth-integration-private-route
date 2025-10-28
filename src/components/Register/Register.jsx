5f719aebf13c1c5721d775e2de11efd24b3bc14e
Al-Amin018555 <alamin15-3799@diu.edu.bd>
2025-10-28 15:13:22 +0600

    firebase work intiated and login register page content added

--- src/components/Register/Register.jsx
+++ src/components/Register/Register.jsx
@@ -1,7 +1,32 @@
+import { Link } from "react-router";
+
 const Register = () => {
+    
+    const handleRegister = e => {
+        e.preventDefault()
+        const name = e.target.name.value;
+        const email = e.target.email.value;
+        const password = e.target.password.value;
+        console.log(name,email,password);
+    }
     return (
-        <div>
-            <h2>This is Register</h2>            
+        <div className="card bg-base-100 w-full mx-auto mt-12 max-w-sm shrink-0 shadow-2xl">
+            <div className="card-body">
+                <h1 className="text-4xl font-bold">Register now!</h1>
+                <form onSubmit={handleRegister} className="fieldset">
+                    <label className="label">Name</label>
+                    <input type="text" name="name" className="input" placeholder="Your Name" />
+
+                    <label className="label">Email</label>
+                    <input type="email" name="email" className="input" placeholder="Email" />
+                    
+                    <label className="label">Password</label>
+                    <input type="password" name="password" className="input" placeholder="Password" />
+                    <div><a className="link link-hover">Forgot password?</a></div>
+                    <button className="btn btn-neutral mt-4">Login</button>
+                </form>
+                <p>Already Have an account? Please <Link to='/login' className="text-blue-400 underline">Login</Link></p>
+            </div>
         </div>
     );
 };
---
+++ src/firebase.init.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBEKXorEg80tgU-QwulLFH4cuypUkQMupU",
+  authDomain: "auth-integration-b3d21.firebaseapp.com",
+  projectId: "auth-integration-b3d21",
+  storageBucket: "auth-integration-b3d21.firebasestorage.app",
+  messagingSenderId: "189978371513",
+  appId: "1:189978371513:web:2eb2927177602021804268"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
