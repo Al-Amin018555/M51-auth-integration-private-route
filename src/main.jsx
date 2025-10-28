e2a5b8f8ee4f6f55925c14125a272ddd5afefb6b
Al-Amin018555 <alamin15-3799@diu.edu.bd>
2025-10-28 14:11:04 +0600

    layout setup done with navbar integration

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,30 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
+import './App.css'
 import { createBrowserRouter, RouterProvider } from 'react-router'
+import Root from './layouts/Root.jsx';
+import Home from './components/Home/Home.jsx';
+import Login from './components/Login/Login.jsx';
+import Register from './components/Register/Register.jsx';
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <div>Hello World</div>,
+    Component: Root,
+    children: [
+      {
+        index: true,
+        Component: Home,
+      },
+      {
+        path: 'login',
+        Component: Login,
+      },
+      {
+        path: 'register',
+        Component: Register,
+      },
+    ]
   },
 ]);
 
